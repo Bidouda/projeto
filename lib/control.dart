107a0efdd4b2af7ca32cccbbcb634ae10960616e
Bidouda <59067642+Bidouda@users.noreply.github.com>
2024-05-24 20:05:37 -0300

    Fix a database bug
    Fix database bug where the sqlite database at startup would not create
    all the necessary tables and would cause the app to misbehave.

--- lib/control.dart
+++ lib/control.dart
@@ -23,22 +23,43 @@ class Control {
 
   Future _onCreate(Database db, int version) async {
     await db.execute(
-      "",
-    );
-  }
-
-  Future insertDatabase(Grupo grupo) async {
-    Database db = await startDatabase();
-    await db.execute(
       """
-      CREATE TABLE IF NOT EXISTS grupos (
+      CREATE TABLE categorias (
+        id_categoria INTEGER PRIMARY KEY AUTOINCREMENT,
+        descricao_categoria VARCHAR(200) NOT NULL
+      );
+      
+      
+      INSERT INTO categorias (descricao_categoria) VALUES ("Paused");
+      INSERT INTO categorias (descricao_categoria) VALUES ("Completed");
+      INSERT INTO categorias (descricao_categoria) VALUES ("Dropped");
+      INSERT INTO categorias (descricao_categoria) VALUES ("Planning");
+      
+      CREATE TABLE grupos (
         id_grupo INTEGER PRIMARY KEY,
         titulo VARCHAR(200) DEFAULT NULL,
         categoria INTEGER NOT NULL,
         FOREIGN KEY (categoria) REFERENCES categorias(id_categoria)
        );
-      """
+      """,
     );
+    await db.execute('INSERT INTO categorias (descricao_categoria) VALUES ("Current");');
+    await db.execute('INSERT INTO categorias (descricao_categoria) VALUES ("Paused");');
+    await db.execute('INSERT INTO categorias (descricao_categoria) VALUES ("Completed");');
+    await db.execute('INSERT INTO categorias (descricao_categoria) VALUES ("Dropped");');
+    await db.execute('INSERT INTO categorias (descricao_categoria) VALUES ("Planning");');
+    await db.execute("""
+      CREATE TABLE grupos (
+        id_grupo INTEGER PRIMARY KEY,
+        titulo VARCHAR(200) DEFAULT NULL,
+        categoria INTEGER NOT NULL,
+        FOREIGN KEY (categoria) REFERENCES categorias(id_categoria)
+      );
+    """);
+  }
+
+  Future insertDatabase(Grupo grupo) async {
+    Database db = await startDatabase();
     String sql = "";
     sql =
     "INSERT INTO grupos (titulo, categoria) VALUES ('${grupo.titulo}', ${grupo.categoria});";
