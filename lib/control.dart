d767650203db721e2b91ba70d1a87d262bcfb05b
Bidouda <59067642+Bidouda@users.noreply.github.com>
2024-05-25 12:21:18 -0300

    Update database to include tables for books
    Update the database to further work on allowing the user to add books
    inside groups.

--- lib/control.dart
+++ lib/control.dart
@@ -28,19 +28,6 @@ class Control {
         id_categoria INTEGER PRIMARY KEY AUTOINCREMENT,
         descricao_categoria VARCHAR(200) NOT NULL
       );
-      
-      
-      INSERT INTO categorias (descricao_categoria) VALUES ("Paused");
-      INSERT INTO categorias (descricao_categoria) VALUES ("Completed");
-      INSERT INTO categorias (descricao_categoria) VALUES ("Dropped");
-      INSERT INTO categorias (descricao_categoria) VALUES ("Planning");
-      
-      CREATE TABLE grupos (
-        id_grupo INTEGER PRIMARY KEY,
-        titulo VARCHAR(200) DEFAULT NULL,
-        categoria INTEGER NOT NULL,
-        FOREIGN KEY (categoria) REFERENCES categorias(id_categoria)
-       );
       """,
     );
     await db.execute('INSERT INTO categorias (descricao_categoria) VALUES ("Current");');
@@ -50,12 +37,47 @@ class Control {
     await db.execute('INSERT INTO categorias (descricao_categoria) VALUES ("Planning");');
     await db.execute("""
       CREATE TABLE grupos (
-        id_grupo INTEGER PRIMARY KEY,
+        id_grupo INTEGER PRIMARY KEY AUTOINCREMENT,
         titulo VARCHAR(200) DEFAULT NULL,
         categoria INTEGER NOT NULL,
         FOREIGN KEY (categoria) REFERENCES categorias(id_categoria)
       );
     """);
+    await db.execute("""
+      CREATE TABLE tipo (
+        id_tipo INTEGER PRIMARY KEY AUTOINCREMENT,
+        descricao_tipo TEXT
+      );
+    """);
+    await db.execute('INSERT INTO tipo (descricao_tipo) VALUES ("Novel");');
+    await db.execute('INSERT INTO tipo (descricao_tipo) VALUES ("Novella");');
+    await db.execute('INSERT INTO tipo (descricao_tipo) VALUES ("Short Story");');
+    await db.execute('INSERT INTO tipo (descricao_tipo) VALUES ("Graphic Novel");');
+    await db.execute('INSERT INTO tipo (descricao_tipo) VALUES ("Drama & Poetry");');
+    await db.execute('''
+      CREATE TABLE autores (
+        id_autor INTEGER PRIMARY KEY AUTOINCREMENT,
+        descricao_autor TEXT
+      );
+    ''');
+    await db.execute('''
+      CREATE TABLE entradas (
+        id_entrada INTEGER PRIMARY KEY AUTOINCREMENT,
+        grupo INTEGER,
+        categoria INTEGER,
+        tipo INTEGER,
+        autor INTEGER,
+        posicao INTEGER,
+        progresso INTEGER,
+        total INTEGER,
+        inicio DATE,
+        fim DATE,
+        FOREIGN KEY (grupo) REFERENCES grupos(id_grupo),
+        FOREIGN KEY (categoria) REFERENCES categorias(id_categoria),
+        FOREIGN KEY (tipo) REFERENCES tipo(id_tipo),
+        FOREIGN KEY (autor) REFERENCES autores(id_autor)
+      );
+    ''');
   }
 
   Future insertDatabase(Grupo grupo) async {
